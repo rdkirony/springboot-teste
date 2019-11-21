bc578328dad7db514c514ebc47c73570965c5f49
rdkirony <gabrielr01oficial@gmail.com>
2019-11-20 20:37:51 -0400

    database

--- src/main/java/com/educandoweb/curso/entities/User.java
+++ src/main/java/com/educandoweb/curso/entities/User.java
@@ -2,10 +2,19 @@ package com.educandoweb.curso.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity; //especificação do jpa
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class User implements Serializable {
 	
 
 	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
